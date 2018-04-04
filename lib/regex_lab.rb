f9d58327811b798072d516a6972e780b0f71f60b
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-04 01:50:29 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,7 @@ end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
 
-  !!text.match(/[A-Z]\w+[[:punct:]]\b/)
+  !!text.match(/[A-Z]\w+[[:punct:]]/)
 end
 
 def valid_phone_number?(phone)
